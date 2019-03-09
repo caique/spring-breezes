026a862a495b631685e0e10b6f7409d75a03b20c
Caique Rodrigues <caique.peixoto@sonicdrivein.com>
2019-03-09 00:33:25 -0300

    [Spring Initializr] Add code for Spring Initializr post.

---
+++ springinitializr/src/test/java/com/breezes/springinitializr/SpringinitializrApplicationTests.java
@@ -0,0 +1,16 @@
+package com.breezes.springinitializr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringinitializrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
